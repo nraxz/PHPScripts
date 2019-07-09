4c01d4763d99c7435762839087f5b05b15e351d3
Naresh Shahi <thenraxz@gmail.com>
2019-07-09 10:30:40 +0100

    Added an arithmatic operation on character variables

--- FunWithString.php
+++ FunWithString.php
@@ -16,3 +16,31 @@ $current_file_name = basename($_SERVER['PHP_SELF']);
 $file_last_modified = filemtime($current_file_name);
 
 echo "Last modified " . date("l, dS F, Y, h:ia", $file_last_modified)."\n";
+
+
+// Arithmetic operation on character variables
+
+$d = 'A0b';
+for ($n=0; $n<5; $n++)
+{
+    echo ++$d ."\n";
+}
+
+
+ /*Output
+    A0c
+    A0d
+    A0e
+    A0f
+    A0g
+  If input $d = 'A0b00'
+
+  then output should be
+    A0b01
+    A0b02
+    A0b03
+    A0b04
+    A0b05
+
+
+ */
